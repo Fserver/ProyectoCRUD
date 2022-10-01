f700f91bfff41c2ea4891953f3a2c64a08c55098
Carlos Felipe Diaz Lombana <server-77@outlook.com>
2022-10-01 13:01:43 -0500

    se habilita buscarVenta y registrarVenta

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let listaClientes = [], campoCedula, campoNombre
+let listaClientes = [], campoCedula, campoNombre, ultimoElementoContador
 
 function leerLocalStorage() {
     listaClientes = []
@@ -32,7 +32,6 @@ function buscarCliente() {
 }
 
 function registrarCliente() {
-
     event.preventDefault()
 
     campoCedula = document.getElementById('campoCedula').value
@@ -51,6 +50,7 @@ function registrarCliente() {
             },)
             localStorage.clear()
             localStorage.setItem(0, JSON.stringify(listaClientes))
+            alert("Se ha registrado el cliente.")
         } else {
             alert("Este cliente ya existe y no puede ser añadido nuevamente")
         }
@@ -136,3 +136,89 @@ function actualizarCliente() {
     });
 
 })()
+
+
+//Aqui se esconde como imprimir todas las ventas por 1 cliente
+function buscarVenta() {
+    event.preventDefault()
+
+    selectCliente = document.getElementById('selectClientes').value
+    //console.log(selectCliente);
+
+    campoID = document.getElementById('idVentas').value
+
+    if (campoID !== "") {
+        listaClientes = leerLocalStorage()
+
+        let registroEncontrado = listaClientes.find(elemento => elemento.cedula == selectCliente)
+        //console.log(registroEncontrado);
+        let ventaEncontrada = registroEncontrado.ventas.find(element => element.id == campoID)
+        console.log(ventaEncontrada);
+
+        if (ventaEncontrada != undefined) {
+            campoProducto = document.getElementById('producto').value = ventaEncontrada.producto
+            campoFecha = document.getElementById('fecha').value = ventaEncontrada.fecha
+            campoValor = document.getElementById('valor').value = ventaEncontrada.valor
+        } else {
+            campoID = document.getElementById('idVentas').value = ""
+            alert("Ese registro no existe")
+        }
+        /*
+            //Sirve para imprimir todas las ventas
+            ventaEncontrada.forEach(element => {
+                console.log(element);
+            });
+        */
+    } else alert("El campo ID es requerido.")
+}
+
+function registrarVenta() {
+    event.preventDefault()
+
+    selectCliente = document.getElementById('selectClientes').value
+    //console.log(selectCliente);
+
+    //campoID = document.getElementById('idVentas').value
+    campoProducto = document.getElementById('producto').value
+    campoFecha = document.getElementById('fecha').value
+    campoValor = document.getElementById('valor').value
+
+    if (campoProducto !== "" && campoFecha !== "" && campoValor !== "") {
+        listaClientes = leerLocalStorage()
+
+        let registroEncontrado = listaClientes.find(elemento => elemento.cedula == selectCliente)
+        //console.log(registroEncontrado);
+
+        let cantidadElementosVentas = registroEncontrado.ventas.length
+        console.log("Cantidad de ventas: ", cantidadElementosVentas + 1);
+
+        try {
+            ultimoElementoContador = registroEncontrado.ventas[cantidadElementosVentas - 1].id
+            console.log("Ultimo id: " + ultimoElementoContador);
+        } catch (error) { }
+
+        if (cantidadElementosVentas <= 0) {
+            registroEncontrado.ventas.push({
+                id: 0,
+                producto: campoProducto,
+                fecha: campoFecha,
+                valor: campoValor
+            },)
+        } else if (cantidadElementosVentas > 0) {
+            registroEncontrado.ventas.push({
+                id: ultimoElementoContador + 1,
+                producto: campoProducto,
+                fecha: campoFecha,
+                valor: campoValor
+            },)
+        }
+
+        localStorage.clear()
+        localStorage.setItem(0, JSON.stringify(listaClientes))
+        alert("Se ha registrado la venta.")
+
+        //console.log(listaClientes);
+
+        document.getElementById('formularioVentas').reset()
+    } else alert("Debe llenar los campos para registrar una venta.")
+}
